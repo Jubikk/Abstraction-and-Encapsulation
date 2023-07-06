5d038d459b3c268fc63c7dfd58fe747136f5d437
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-07-07 05:15:17 +0800

    added class and def car

--- CAR/CarTest.py
+++ CAR/CarTest.py
@@ -1,9 +1,13 @@
 #Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.9 | CAR |
 
-#import Class 
+#import Class
+ from CarClass import CAR
 
 #def "car"
+def car():
+ 
    #The_Car = Car(,)
+   The_Car = CAR(2004, "Subaru")
 
 #Use range for both acceleration and brake
 
