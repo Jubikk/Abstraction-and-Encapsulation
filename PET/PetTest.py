8376853fa06b743ea750f6af219bd032b01e080f
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-07-07 06:04:49 +0800

    imported the class and creating input function aswell

--- PET/PetTest.py
+++ PET/PetTest.py
@@ -1,11 +1,15 @@
 # Joebeck Andrew F. Gusi | BSCPE 1-5 | Assigment No.9 | PET |
 
 #import the PetClass
+from PetClass import PET
 
 #input function for:
 #name
+NAME = input("Please enter the name of your pet:")
 #age
+AGE = input("Please enter the age of your pet:")
 #animal_type
+TYPE = input("Please enter the type of animal is your pet:")
 
 
 #set the inputted values
