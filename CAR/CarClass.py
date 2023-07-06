e7c24fc422d7731970173e4800ea858401fb229f
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-07-07 04:55:12 +0800

    added def

--- CAR/CarClass.py
+++ CAR/CarClass.py
@@ -7,3 +7,13 @@ class CAR:
         self.__make = make
         self.__speed = 0
 
+#define accelerate, brake, speed
+    def accelerate(self):
+        self.__speed += 5
+    def brake(self):
+        self.__speed -= 5
+    def get_speed(self):
+        return self.__speed
+
+
+#end
