a1956870b254002616822db22ec62e8e083be184
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-07-07 04:48:48 +0800

    added class

--- CAR/CarClass.py
+++ CAR/CarClass.py
@@ -1,8 +1,9 @@
 #Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.9 | CAR |
 
 #Create Car Class
+class CAR:
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
 
-#define accelerate, brake, speed
-
-
-#end
