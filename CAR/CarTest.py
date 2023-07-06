2fd22f89aa1153b0c25de77aa29abd3a8e13088a
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-07-07 05:28:11 +0800

    added the rest of the code (range)

--- CAR/CarTest.py
+++ CAR/CarTest.py
@@ -1,7 +1,7 @@
 #Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.9 | CAR |
 
 #import Class
- from CarClass import CAR
+from CarClass import CAR
 
 #def "car"
 def car():
@@ -10,4 +10,16 @@ def car():
    The_Car = CAR(2004, "Subaru")
 
 #Use range for both acceleration and brake
+   for i in range(5):
+       The_Car.accelerate()
+       print("The current speed is:", The_Car.get_speed())
+    
+   for i in range(5):
+       The_Car.brake()
+       print("The current speed is:", The_Car.get_speed())
+
+if __name__== "__main__" :
+    car()
+
+#end
 
