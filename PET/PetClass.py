faeb30d73040dc23fa848db2036327b8faa3dfc4
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-07-07 05:46:13 +0800

    added class pet

--- PET/PetClass.py
+++ PET/PetClass.py
@@ -1,7 +1,13 @@
 # Joebeck Andrew F. Gusi | BSCPE 1-5 | Assigment No.9 | PET |
 
 #Create a class pet
+class PET:
+
     #def init
+    def __init__(self):
+        self.__name = ""
+        self.__age = 0
+        self.__animal_type = ""
 
     #set name, age, animal_type
     #get name, age, animal_type
