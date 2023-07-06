e5084d41264373c5a9a763196dc5050b449ee783
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-07-07 05:49:45 +0800

    added setter and getter

--- PET/PetClass.py
+++ PET/PetClass.py
@@ -11,6 +11,20 @@ class PET:
 
     #set name, age, animal_type
     #get name, age, animal_type
+    def set_name(self, name):
+        self.__name = name
+    def get_name(self):
+        return self.__name
+    
+    def set_age(self, age):
+        self.__age = age
+    def get_age(self):
+        return self.__age
+    
+    def set_animal_type(self, animal_type): 
+        self.__animal_type = animal_type
+    def get_animal_type(self):
+        return self.__animal_type
 
 #end
 
