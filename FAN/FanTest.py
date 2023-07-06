f7df290c5069c03a0c30513d28f099cb3f183920
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-07-07 02:23:34 +0800

    added display for fan1 and 2

--- FAN/FanTest.py
+++ FAN/FanTest.py
@@ -9,5 +9,19 @@ fan1 = FAN(FAN.Fast, 10, 'yellow', True)
 #Second Fan
 fan2 = FAN(FAN.Medium, 5, 'blue', False)
 
+#Print or Display the first fan
+print("FAN_1:")
+print("-SPEED:", fan1.get_speed())
+print("-RADIUS:", fan1.get_radius())
+print("-COLOR:", fan1.get_color())
+print("-ON:", fan1.get_on())
 
+#Print or Display the second fan
+print("FAN_2:")
+print("-SPEED:", fan2.get_speed())
+print("-RADIUS:", fan2.get_radius())
+print("-COLOR:", fan2.get_color())
+print("-ON:", fan2.get_on())
+
+#end 
 
