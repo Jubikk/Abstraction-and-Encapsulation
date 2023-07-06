1ecb085df906f0ead119e1d6c2b090a5e5c2b75c
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-07-07 06:01:38 +0800

    Pseudocode for Pet Test

--- PET/PetTest.py
+++ PET/PetTest.py
@@ -1,1 +1,17 @@
 # Joebeck Andrew F. Gusi | BSCPE 1-5 | Assigment No.9 | PET |
+
+#import the PetClass
+
+#input function for:
+#name
+#age
+#animal_type
+
+
+#set the inputted values
+
+
+#print/display the details
+
+
+#end
